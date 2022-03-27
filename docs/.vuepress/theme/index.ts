70f714b2ad46ce64ea6e21b588e74aea4281bf8f
Xiaoyun Wu <xiaoyun.wu@gmail.com>
2022-03-26 18:13:48 -0700

    use new theme

---
+++ docs/.vuepress/theme/index.ts
@@ -0,0 +1,10 @@
+
+import type { ThemeObject } from '@vuepress/core'
+import { path } from '@vuepress/utils'
+
+const localTheme: ThemeObject = {
+  name: 'vuepress-theme-local',
+  extends: '@lando/vuepress-theme-default-plus',
+}
+
+export default localTheme
