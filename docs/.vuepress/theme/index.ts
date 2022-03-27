780ac0682ba0679eea88406237061a6b41a444d2
Xiaoyun Wu <xiaoyun.wu@gmail.com>
2022-03-26 19:51:32 -0700

    use hope by default

---
+++ docs/.vuepress/theme/index.ts
@@ -0,0 +1,9 @@
+import type { ThemeObject } from '@vuepress/core'
+import { path } from '@vuepress/utils'
+
+const localTheme: ThemeObject = {
+  name: 'vuepress-theme-local',
+  extends: '@vuepress/hope',
+}
+
+export default localTheme
