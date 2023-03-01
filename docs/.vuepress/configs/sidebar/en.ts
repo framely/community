01b0e00e323ff225ef7efb01ff457baa542b1903
Sunny <hisunnymay@163.com>
2023-03-01 18:04:03 +0800

    Add reservation-api.md

--- docs/.vuepress/configs/sidebar/en.ts
+++ docs/.vuepress/configs/sidebar/en.ts
@@ -113,7 +113,8 @@ export const en = {
           text: 'Reservation',
           children:[
             '/plugins/services/reservation/reservation-requirement.md',
-            '/plugins/services/reservation/google-calendar-reservation.md'
+            '/plugins/services/reservation/google-calendar-reservation.md',
+            '/plugins/services/reservation/reservation-api.md'
           ],
         }
       ]
