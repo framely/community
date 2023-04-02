2fcfbfdfd94bd9b3a29b8b1538827606ef8569e2
ChangMei <hisunnymay@163.com>
2023-04-02 20:33:19 +0800

    fix en.tx

--- docs/.vuepress/configs/sidebar/en.ts
+++ docs/.vuepress/configs/sidebar/en.ts
@@ -24,7 +24,6 @@ export const en = {
         '/guide/debug.md',
         '/guide/pingpong.md',
         '/guide/use-hours.md',
-        '/guide/quickstart-reservation.md',
         '/guide/build-service.md',
         '/guide/quickstart-channel.md'
       ],
