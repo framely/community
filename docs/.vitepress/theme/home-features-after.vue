a6294ba304c4ac6bf27cf0fa7d9521f9cc19b23d
fangyi <jfang137@163.com>
2023-09-15 14:30:11 +0800

    home features after

---
+++ docs/.vitepress/theme/home-features-after.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { useData } from 'vitepress';
+const { page, frontmatter } = useData()
+
+console.log('frontmatter:', frontmatter);
+import Cta from '../../components/cta/callToAction.vue'
+import ContentCard from '../../components/contentCard/ContentCard.vue'
+import Video from '../../components/video/videoSection.vue'
+import CFooter from '../../components/footer/columnFooter.vue'
+
+</script>
+
+<template #home-features-after>
+  <Video />
+  <ContentCard />
+  <Cta />
+  <CFooter />
+</template>
+
+<style lang="scss">
+.home-features-after {
+  width: 100%;
+  height: 200px;
+  background: red;
+}
+</style>
+
