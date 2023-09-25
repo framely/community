ca77c308db24b17276367d75b50bd73d608efded
xiaoyun wu <xiaoyun.wu@gmail.com>
2023-09-25 20:46:15 +0100

    Update nav-content-after.vue

--- docs/.vitepress/theme/nav-content-after.vue
+++ docs/.vitepress/theme/nav-content-after.vue
@@ -18,6 +18,10 @@ const toggleAppearance = function (v) {
       Request Access
     </button>
   </a>
+  <VPSwitch title="toggle dark mode" class="VPSwitchAppearance" :aria-checked="isDark" @click="toggleAppearance">
+    <VPIconSun class="sun" />
+    <VPIconMoon class="moon" />
+  </VPSwitch>
 </template>
 
 <style lang="scss">
